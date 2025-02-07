8a0445599cbe23a49878a233bb5d07528210ba7a
Daniel <38142096+adaniel2@users.noreply.github.com>
2025-02-06 22:08:08 -0500

    fixing docker issues and added main.yml file for github workflow

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react(), tailwindcss()],
   host: true,
   preview: {
-   port: 8081,
+   port: 5173,
    strictPort: true,
   },
   server: {
-   port: 8081,
+   port: 5173,
    strictPort: true,
    host: true,
-   origin: "http://0.0.0.0:8081",
+   origin: "http://0.0.0.0:5173",
   },
  });
  
